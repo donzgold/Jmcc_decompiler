4b375ff47404aa98480986277b5c1db27f8d01d7
donzgold <dr.donzgold@yandex.ru>
2023-11-17 18:14:40 +0500

    0.1

--- main.cmd
+++
@@ -1,6 +0,0 @@
-
-@echo off
-rem This script was created by Nuitka to execute 'main.exe' with Python DLL being found.
-set PATH=c:\users\djiri\appdata\local\programs\python\PYTHON~1;%PATH%
-set PYTHONHOME=C:\Users\djiri\AppData\Local\Programs\Python\Python310
-"%~dp0.\main.exe" %*
